bb79ecadf971cec16eb9342250e563aabe4972a2
LiamAbyss <paul.bucamp@isen.yncrea.fr>
2021-03-23 16:32:40 +0100

    Add some compatibility with three.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import spline from './utils/spline.mjs'
+import {spline, toVector3} from './utils/spline.js'
 
 let controlPoints = [
-  [-1.0,  0.0],
-  [-0.5,  0.5],
-  [ 0.5, -0.5],
-  [ 1.0,  0.0],
-  // Repeat the degree + 1 points for a closed curve
-  [-1.0,  0.0],
-  [-0.5,  0.5],
-  [ 0.5, -0.5]
+  [0.0,  0.0, 0],
+  [-0.6,  0.7, 0],
+  [-1,  1.5, 0],
+  [-0.5,  2, 0],
+  [0,  1.5, 0],
+  [0.5,  2, 0],
+  [1,  1.5, 0],
+  [0.6,  0.7, 0],
+  [0.0,  0.0, 0],
+  [-0.6,  0.7, 0],
+  [-1,  1.5, 0],
 ];
 
+let controlPoints2 = toVector3(controlPoints)
+controlPoints2.forEach(elt => { elt.x -= 2 })
 
 ReactDOM.render(
   <React.StrictMode>
@@ -39,9 +44,25 @@ function graph() {
 
   theContext.beginPath();
   theContext.moveTo(points[0][0] * 50 + 250, -points[0][1] * 50 + 250);
-  for (var x = 1; x < points.length; x++) {
+  let x
+  for (x = 1; x < points.length; x++) {
     theContext.lineTo((points[x][0]) * 50 + 250, -points[x][1] * 50 + 250);
   }
   theContext.stroke();
+  theContext.closePath()
+
+  controlPoints2.forEach(elt => {
+    theContext.fillRect(elt.x * 50 + 250 - 3, -elt.y * 50 + 250 - 3, 6, 6)
+  })
+
+  points = spline(2, controlPoints2)
+
+  theContext.beginPath();
+  theContext.moveTo(points[0].x * 50 + 250, -points[0].y * 50 + 250);
+  for (x = 1; x < points.length; x++) {
+    theContext.lineTo((points[x].x) * 50 + 250, -points[x].y * 50 + 250);
+  }
+  theContext.stroke();
+  theContext.closePath()
 }
 
